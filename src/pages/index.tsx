7244127a434a91911db45070a3014a080ef024ec
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-11 15:53:59 +0530

    implement auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
 const Home = () => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <main>Loading...</main>;
+  }
+
   return (
     <main>
       <h1>Guestbook</h1>
+      {session ? (
+        <div>
+          <p>
+            hi {session.user?.name}
+          </p>
+
+          <button onClick={() => signOut()}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => signIn("discord")}>Login with Discord</button>
+        </div>
+      )}
     </main>
   );
 };
