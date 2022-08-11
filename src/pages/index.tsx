342391e81a9dbba2fa90d7424110030fd38543f7
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-11 19:06:36 +0530

    add basic styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,25 +4,31 @@ const Home = () => {
   const { data: session, status } = useSession();
 
   if (status === "loading") {
-    return <main>Loading...</main>;
+    return <main className="flex flex-col items-center pt-4">Loading...</main>;
   }
 
   return (
-    <main>
-      <h1>Guestbook</h1>
-      {session ? (
-        <div>
-          <p>
-            hi {session.user?.name}
-          </p>
+    <main className="flex flex-col items-center">
+      <h1 className="text-3xl pt-4">Guestbook</h1>
+      <p>
+        Tutorial for <code>create-t3-app</code>
+      </p>
 
-          <button onClick={() => signOut()}>Logout</button>
-        </div>
-      ) : (
-        <div>
-          <button onClick={() => signIn("discord")}>Login with Discord</button>
-        </div>
-      )}
+      <div className="pt-10">
+        {session ? (
+          <div>
+            <p>hi {session.user?.name}</p>
+
+            <button onClick={() => signOut()}>Logout</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={() => signIn("discord")}>
+              Login with Discord
+            </button>
+          </div>
+        )}
+      </div>
     </main>
   );
 };
